06904e4bf7b92b8a08bebfd95b4ffd2337c98912
andrey-zh <87668873+andrey-zh@users.noreply.github.com>
2021-11-18 15:33:09 +0200

    Update and rename install-helm.sh to install-helm+nginx.sh

--- install-helm+nginx.sh
+++ install-helm+nginx.sh
@@ -2,3 +2,5 @@
 wget https://get.helm.sh/helm-v3.7.1-linux-amd64.tar.gz
 tar xfz helm-v3.7.1-linux-amd64.tar.gz
 cp linux-amd64/helm /usr/local/bin/
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm install nginx-ingress ingress-nginx/ingress-nginx
